abe0c2a0f891bc4a8b8df43006547d9746469cc1
Johnny Ferreira da Silva <joferres@everis.com>
2021-08-06 20:23:17 -0300

    adicionando theme chakra

---
+++ styles/theme.ts
@@ -0,0 +1,28 @@
+import { extendTheme } from '@chakra-ui/react';
+
+
+
+export const theme = extendTheme({
+
+    colors: {
+        gray: {
+            "900": "#47585B",
+            "300": "#999999"
+        },
+        white: {
+            "100": "#F5F8FA"
+        }
+    },
+    fonts: {
+        heading: 'Poppins',
+        body: 'Poppins',
+    },
+    styles: {
+        global: {
+            body: {
+                bg: 'white.100',
+                color: 'gray.900',
+            }
+        }
+    }
+})
